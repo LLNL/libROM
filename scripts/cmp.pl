9f39df013f27823d3a633bc77877f2c7369b983f
William J. Arrighi <arrighi2@sierra324.llnl.gov>
2014-10-09 13:02:03 -0700

    Add dependency file and scripts to generate it.

---
+++ scripts/cmp.pl
@@ -0,0 +1,24 @@
+#!/usr/local/bin/perl
+## Usage: cmp.pl <file1> <file2>
+##
+
+$ANAME = shift(@ARGV);
+$BNAME = shift(@ARGV);
+
+open(AFILE, "$ANAME") || die "Cannot open input file $ANAME...";
+open(BFILE, "$BNAME") || die "Cannot open input file $BNAME...";
+
+while (!eof(AFILE) && !eof(BFILE)) {
+   $ALINE = <AFILE>;
+   $BLINE = <BFILE>;
+   $_ = $ALINE;
+
+   if (!/^(\/\/|c|C|#|##| \*)[ ]*(Release:[\t ]*\$Name|Revision:[\t ]*\$LastChangedRevision|Modified:[\t ]*\$LastChangedDate):[^\$]*\$/o) {
+      if ($ALINE ne $BLINE) {
+         exit 1;
+      }
+   }
+}
+
+exit 0 if (eof(AFILE) && eof(BFILE));
+exit 1;
