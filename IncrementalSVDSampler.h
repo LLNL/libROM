f0c6df82b000b8d868ae24f69d2e2f6de1f23f64
William J. Arrighi <arrighi2@sierra0.llnl.gov>
2016-04-05 15:59:34 -0700

    Prepare for release under BSD opensource license.

--- IncrementalSVDSampler.h
+++ IncrementalSVDSampler.h
@@ -1,14 +1,45 @@
 /******************************************************************************
  *
- * This file is part of the CAROM distribution.  For full copyright
- * information, see COPYRIGHT.
+ * Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+ * Produced at the Lawrence Livermore National Laboratory
+ * Written by William Arrighi wjarrighi@llnl.gov
+ * CODE-686965
+ * All rights reserved.
  *
- * Copyright:   (c) 2013-2015 Lawrence Livermore National Security, LLC
- * Description: The class that determines the next time at which a sample
- *              should be taken for basis generation using an incremental SVD
- *              approach.
+ * This file is part of libROM.
+ * For details, see https://computation.llnl.gov/librom
+ * Please also read README_BSD_NOTICE.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modifications, are permitted provided that the following conditions are met:
+ *
+ *    o Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the disclaimer below.
+ *    o Redistribution in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the disclaimer (as noted below) in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *    o Neither the name of the LLNS/LLNL nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL LAWRENCE LIVERMORE NATIONAL SECURITY,
+ * LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OR SUCH DAMAGE.
  *
  *****************************************************************************/
+
+// Description: The class that determines the next time at which a sample
+//              should be taken for basis generation using an incremental SVD
+//              approach.
 
 #ifndef included_IncrementalSVDSampler_h
 #define included_IncrementalSVDSampler_h
