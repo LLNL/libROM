4196384877ffd700c3df89eb9b3516209b94e86f
Geoffrey Malcolm Oxberry <goxberry@gmail.com>
2018-03-02 03:07:43 -0800

    Matrix: delegate {un}balanced distributed QRCP

--- Matrix.h
+++ Matrix.h
@@ -634,7 +634,7 @@ class Matrix
        * this Matrix, if it is distributed and balanced. Prototype
        * using Elemental (requires C++11).
        *
-       * @pre distributed() && balanced()
+       * @pre distributed()
        *
        * @param[out] row_pivot Array of leading column pivots
        * from QRCP of transpose of this Matrix, has length pivots_requested
@@ -647,6 +647,45 @@ class Matrix
       qrcp_pivots_transpose_distributed_elemental(int* row_pivot,
 						  int* row_pivot_owner,
 						  int  pivots_requested) const;
+
+      /**
+       * @brief Compute the leading column pivots from a QR
+       * decomposition with column pivots (QRCP) of the transpose of
+       * this Matrix, if it is distributed and balanced. Prototype
+       * using Elemental (requires C++11).
+       *
+       * @pre distributed() && balanced()
+       *
+       * @param[out] row_pivot Array of leading column pivots
+       * from QRCP of transpose of this Matrix, has length pivots_requested
+       * @param[out] row_pivot_owner Array of process rank that owns
+       * each pivot on the communicator owned by this Matrix.
+       * @param[in]  number of pivots requested, must be less than or equal
+       * to the number of rows of this Matrix.
+       */
+      void
+      qrcp_pivots_transpose_distributed_elemental_balanced
+	(int* row_pivot, int* row_pivot_owner, int pivots_requested) const;
+
+      /**
+       * @brief Compute the leading column pivots from a QR
+       * decomposition with column pivots (QRCP) of the transpose of
+       * this Matrix, if it is distributed and balanced. Prototype
+       * using Elemental (requires C++11).
+       *
+       * @pre distributed() && !balanced()
+       *
+       * @param[out] row_pivot Array of leading column pivots
+       * from QRCP of transpose of this Matrix, has length pivots_requested
+       * @param[out] row_pivot_owner Array of process rank that owns
+       * each pivot on the communicator owned by this Matrix.
+       * @param[in]  number of pivots requested, must be less than or equal
+       * to the number of rows of this Matrix.
+       */
+      void
+      qrcp_pivots_transpose_distributed_elemental_unbalanced
+	(int* row_pivot, int* row_pivot_owner, int pivots_requested) const;
+
 
       /**
        * @brief The storage for the Matrix's values on this processor.
