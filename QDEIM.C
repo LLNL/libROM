856e0e571dc3e0d45927056001f73f98ef822644
Geoffrey Malcolm Oxberry <goxberry@gmail.com>
2018-03-01 16:23:54 -0800

    QDEIM: add serial implementation

---
+++ QDEIM.C
@@ -0,0 +1,89 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2013-2017, Lawrence Livermore National Security, LLC.
+ * Produced at the Lawrence Livermore National Laboratory
+ * Written by William Arrighi wjarrighi@llnl.gov
+ * CODE-686965
+ * All rights reserved.
+ *
+ * This file is part of libROM.
+ * For details, see https://computation.llnl.gov/librom
+ * Please also read README_BSD_NOTICE.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modifications, are permitted provided that the following conditions are met:
+ *
+ *    o Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the disclaimer below.
+ *    o Redistribution in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the disclaimer (as noted below) in
+ *      the documentation and/or other materials provided with the
+ *      distribution.
+ *    o Neither the name of the LLNS/LLNL nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL LAWRENCE LIVERMORE NATIONAL SECURITY,
+ * LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OR SUCH DAMAGE.
+ *
+ *****************************************************************************/
+
+// Description: Interface to the DEIM algorithm to determine the rows of the
+// rhs to be sampled for the interpolation of the rhs.
+
+#include "Matrix.h"
+#include "mpi.h"
+#include <cmath>
+#include <vector>
+
+namespace CAROM {
+
+  /* Implement QDEIM algorithm from Zlatko Drmac, Serkan Gugercin, "A
+     new selection operator for the discrete empirical interpolation
+     method -- Improved a priori error bound and extensions", SIAM
+     Journal on Scientific Computing, Volume 38, Issue 2, pages
+     A631-A648, 2016.
+  */
+void
+QDEIM(const Matrix* f_basis,
+      int num_f_basis_vectors_used,
+      int* f_sampled_row,
+      int* f_sampled_row_owner,
+      Matrix& f_basis_sampled,
+      int myid)
+{
+  // This algorithm determines the rows of f that should be sampled, the
+  // processor that owns each sampled row, and fills f_basis_sampled with the
+  // sampled rows of the basis of the RHS.
+
+  // For now, implement QDEIM in serial, then make a parallel
+  // implementation.
+  CAROM_ASSERT(!f_basis->distributed());
+
+  // QDEIM computes selection/interpolation indices by taking a
+  // column-pivoted QR-decomposition of the transpose of its input
+  // matrix.
+  std::vector<int> indices;
+  f_basis->qrcp_pivots_transpose(indices);
+
+  // With the known interpolation (sample) indices, copy over the
+  // rows of the sampled basis
+  for (int i = 0; i < indices.size(); i++) {
+    f_sampled_row[i] = indices[i];
+    for (int j = 0; j < indices.size(); j++) {
+      f_basis_sampled.item(i, j) = f_basis->item(f_sampled_row[i], j);
+    }
+  }
+
+} // end void QDEIM
+
+} // end namespace CAROM
