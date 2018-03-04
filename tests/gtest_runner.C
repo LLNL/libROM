676dc55a4b7992cd82191530d74841667e310940
Geoffrey Malcolm Oxberry <goxberry@gmail.com>
2018-03-04 14:48:25 -0800

    testing: add test for serial QRCP

--- tests/gtest_runner.C
+++ tests/gtest_runner.C
@@ -41,6 +41,93 @@
 // Framework to run unit tests.
 
 #include<gtest/gtest.h>
+#include <mpi.h>
+#include "../Matrix.h"
+
+// Test fixture for testing
+TEST(MatrixSerialQRCPTest, SecondDifferenceMatrix) {
+  // Allocate space for second_difference matrix
+  const int size = 4;
+  const bool is_distributed = false;
+  CAROM::Matrix second_difference(size, size, is_distributed);
+
+  /* A "second difference" matrix is a tridiagonal matrix that looks like:
+
+     [ 2 -1         ]
+     [-1  2 -1      ]
+     [   .. .. ..   ]
+     [      -1  2 -1]
+     [         -1  2]
+
+     which looks like the stencil from second-order central
+     differencing (or the stiffness matrix for finite elements on a
+     subspace of H1 with linear basis functions) for Poisson's
+     equation, modulo boundary conditions (that would be applied to
+     the first and last rows).
+   */
+
+  // The code below won't work if the size is less than 2.
+  assert(size >= 2);
+
+  // Zero out the entire matrix
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      second_difference.item(i, j) = 0;
+    }
+  }
+
+  // Build first row of second difference matrix
+  second_difference.item(0, 0) =  2;
+  second_difference.item(0, 1) = -1;
+
+  // Build second through penultimate rows of
+  // second difference matrix
+  for (int row = 1; row < size - 1; row++) {
+    second_difference.item(row, row - 1) = -1;
+    second_difference.item(row, row    ) =  2;
+    second_difference.item(row, row + 1) = -1;
+  }
+
+  // Build last row of second difference matrix
+  second_difference.item(size - 1, size - 2) = -1;
+  second_difference.item(size - 1, size - 1) =  2;
+
+  // Allocate memory for QRCP arguments
+  int* row_pivot                 =       new int[size];
+  int* row_pivot_owner           =       new int[size];
+  const int row_pivots_requested =               size ;
+
+  // Compute pivots
+  second_difference.qrcp_pivots_transpose(row_pivot,
+					  row_pivot_owner,
+					  row_pivots_requested);
+
+  // All row pivot owners should be on the current rank;
+  // all row pivots shouold be less than the size of the matrix
+  int is_mpi_initialized, is_mpi_finalized;
+  CAROM_ASSERT(MPI_Initialized(&is_mpi_initialized) == MPI_SUCCESS);
+  CAROM_ASSERT(MPI_Finalized(&is_mpi_finalized) == MPI_SUCCESS);
+  int my_rank = 0;
+  if(is_mpi_initialized && !is_mpi_finalized) {
+    const MPI_Comm my_comm = MPI_COMM_WORLD;
+    CAROM_ASSERT(MPI_Comm_rank(my_comm, &my_rank) == MPI_SUCCESS);
+  }
+
+  for (int i = 0; i < row_pivots_requested; i++) {
+    EXPECT_EQ(row_pivot_owner[i], my_rank);
+    EXPECT_TRUE(row_pivot[i] < size);
+  }
+
+  // Row pivots should be known values
+  EXPECT_EQ(row_pivot[0], 1);
+  EXPECT_EQ(row_pivot[1], 3);
+  EXPECT_EQ(row_pivot[2], 0);
+  EXPECT_EQ(row_pivot[3], 2);
+
+  // Free allocated arrays
+  delete [] row_pivot;
+  delete [] row_pivot_owner;
+}
 
 // Simple smoke test to make sure runner is properly linked
 TEST(GoogleTestFramework, GoogleTestFrameworkFound) {
