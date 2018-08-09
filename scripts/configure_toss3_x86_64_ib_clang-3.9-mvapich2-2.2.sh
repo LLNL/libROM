e87ba4bae3b2d968f1c6984b9f3cdc867fd04b13
Geoffrey M. Oxberry <oxberry1@llnl.gov>
2018-08-08 18:03:17 -0700

    Add configure script for TOSS3/MVAPICH2/Clang 3.9

---
+++ scripts/configure_toss3_x86_64_ib_clang-3.9-mvapich2-2.2.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./configure --with-CXX="/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.1/bin/mpicxx" --with-lapack=/usr/gapps/bdiv/toss_3_x86_64_ib/clang-3.9-mvapich2-2.2/lapack/3.5.0/lib --with-hdf5=/usr/gapps/bdiv/toss_3_x86_64_ib/clang-3.9-mvapich2-2.2/hdf5/1.8.10p1 --with-zlib=/usr/gapps/bdiv/toss_3_x86_64_ib/clang-3.9-mvapich2-2.2/zlib/1.2.3/lib --with-gtest=/usr/gapps/bdiv/toss_3_x86_64_ib/clang-3.9-mvapich2-2.2/gtest/1.8.0 --with-elemental=no --enable-opt=yes --enable-debug=no "$@"
