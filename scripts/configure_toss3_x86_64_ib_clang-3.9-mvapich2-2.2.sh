6056385ec3827bb93366e8e45d5968c0a345b64a
Geoffrey M. Oxberry <oxberry1@llnl.gov>
2018-08-17 03:00:16 -0700

    configure: format TOSS3/Clang 3.9/MVAPICH2 script

--- scripts/configure_toss3_x86_64_ib_clang-3.9-mvapich2-2.2.sh
+++ scripts/configure_toss3_x86_64_ib_clang-3.9-mvapich2-2.2.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-./configure --with-CXX="/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.1/bin/mpicxx" --with-FC="/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.1/bin/mpif90" --with-lapack=/usr/gapps/bdiv/toss_3_x86_64_ib/clang-3.9-mvapich2-2.2/lapack/3.5.0/lib --with-hdf5=/usr/gapps/bdiv/toss_3_x86_64_ib/clang-3.9-mvapich2-2.2/hdf5/1.8.10p1 --with-zlib=/usr/gapps/bdiv/toss_3_x86_64_ib/clang-3.9-mvapich2-2.2/zlib/1.2.3/lib --with-gtest=/usr/gapps/bdiv/toss_3_x86_64_ib/clang-3.9-mvapich2-2.2/gtest/1.8.0 --with-elemental=no --enable-opt=yes --enable-debug=no "$@"
+export PKG_ROOT=/usr/gapps/bdiv/toss_3_x86_64_ib/clang-3.9-mvapich2-2.2
+export COMPILER_ROOT=/usr/tce/packages/mvapich2/mvapich2-2.2-clang-3.9.1/bin
+./configure \
+    --with-CXX=${COMPILER_ROOT}/mpicxx \
+    --with-FC=${COMPILER_ROOT}/mpif90 \
+    --with-lapack=${PKG_ROOT}/lapack/3.5.0/lib \
+    --with-hdf5=${PKG_ROOT}/hdf5/1.8.10p1 \
+    --with-zlib=${PKG_ROOT}/zlib/1.2.3/lib \
+    --with-gtest=${PKG_ROOT}/gtest/1.8.0 \
+    --with-elemental=no \
+    --enable-opt=yes \
+    --enable-debug=no \
+    "$@"
