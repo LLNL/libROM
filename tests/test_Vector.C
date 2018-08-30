3048563ad694bef9237c53e2719526488d76ef22
Geoffrey Malcolm Oxberry <goxberry@gmail.com>
2018-08-30 15:58:46 -0700

    Vector: test copy-assignment operator

--- tests/test_Vector.C
+++ tests/test_Vector.C
@@ -163,6 +163,18 @@ TEST(VectorSerialTest, Test_copy_constructor)
   EXPECT_DOUBLE_EQ(w(1), 2);
 }
 
+TEST(VectorSerialTest, Test_copy_assignment_operator)
+{
+  double v_data[2] = {1, 2};
+  const CAROM::Vector v(v_data, 2, false, true);
+  CAROM::Vector w = v;
+
+  EXPECT_FALSE(w.distributed());
+  EXPECT_EQ(w.dim(), 2);
+  EXPECT_DOUBLE_EQ(w(0), 1);
+  EXPECT_DOUBLE_EQ(w(1), 2);
+}
+
 
 /** Test methods that operate on Vector objects
  *
