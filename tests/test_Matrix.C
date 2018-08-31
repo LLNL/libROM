65d7e08a8c1ac61ccadee95edf73e6674ce66811
Geoffrey Malcolm Oxberry <goxberry@gmail.com>
2018-08-30 18:56:44 -0700

    Matrix: test all multTranspose methods

--- tests/test_Matrix.C
+++ tests/test_Matrix.C
@@ -1062,6 +1062,129 @@ TEST(MatrixSerialTest, Test_mult_Vector_Vector_reference)
   EXPECT_DOUBLE_EQ(w(1), 6.0);
 }
 
+TEST(MatrixSerialTest, Test_transposeMult_Vector_reference)
+{
+ /**
+   *  Build matrix [ 1.0   0.0]
+   *               [ 1.0   1.0]
+   *
+   */
+  double asymmetric[4] = {1.0, 0.0, 1.0, 1.0};
+  CAROM::Matrix asymmetric_matrix(asymmetric, 2, 2, false, true);
+
+  /**
+   *  Build vector [ 2.0]
+   *               [ 4.0]
+   */
+  double v_data[2] = {2.0, 4.0};
+  CAROM::Vector v(v_data, 2, false, true);
+
+ /**
+   *  [ 1.0   0.0]^{T} [ 2.0] = [ 6.0]
+   *  [ 1.0   1.0]     [ 4.0]   [ 4.0]
+   *
+   */
+  CAROM::Vector *w;
+  w = asymmetric_matrix.transposeMult(v);
+  EXPECT_FALSE(w->distributed());
+  EXPECT_EQ(w->dim(), 2);
+  EXPECT_DOUBLE_EQ((*w)(0), 6.0);
+  EXPECT_DOUBLE_EQ((*w)(1), 4.0);
+  delete w;
+}
+
+TEST(MatrixSerialTest, Test_transposeMult_Vector_pointer)
+{
+ /**
+   *  Build matrix [ 1.0   0.0]
+   *               [ 1.0   1.0]
+   *
+   */
+  double asymmetric[4] = {1.0, 0.0, 1.0, 1.0};
+  CAROM::Matrix asymmetric_matrix(asymmetric, 2, 2, false, true);
+
+  /**
+   *  Build vector [ 2.0]
+   *               [ 4.0]
+   */
+  double v_data[2] = {2.0, 4.0};
+  CAROM::Vector *v = new CAROM::Vector(v_data, 2, false, true);
+
+  /**
+   *  [ 1.0   0.0]^{T} [ 2.0] = [ 6.0]
+   *  [ 1.0   1.0]     [ 4.0]   [ 4.0]
+   *
+   */
+  CAROM::Vector *w;
+  w = asymmetric_matrix.transposeMult(v);
+  EXPECT_FALSE(w->distributed());
+  EXPECT_EQ(w->dim(), 2);
+  EXPECT_DOUBLE_EQ((*w)(0), 6.0);
+  EXPECT_DOUBLE_EQ((*w)(1), 4.0);
+  delete w; delete v;
+}
+
+TEST(MatrixSerialTest, Test_transposeMult_Vector_Vector_pointer)
+{
+ /**
+   *  Build matrix [ 1.0   0.0]
+   *               [ 1.0   1.0]
+   *
+   */
+  double asymmetric[4] = {1.0, 0.0, 1.0, 1.0};
+  CAROM::Matrix asymmetric_matrix(asymmetric, 2, 2, false, true);
+
+  /**
+   *  Build vector [ 2.0]
+   *               [ 4.0]
+   */
+  double v_data[2] = {2.0, 4.0};
+  CAROM::Vector v(v_data, 2, false, true);
+
+  /**
+   *  [ 1.0   0.0]^{T} [ 2.0] = [ 6.0]
+   *  [ 1.0   1.0]     [ 4.0]   [ 4.0]
+   *
+   */
+  CAROM::Vector *w = NULL;
+  asymmetric_matrix.transposeMult(v, w);
+  EXPECT_FALSE(w->distributed());
+  EXPECT_EQ(w->dim(), 2);
+  EXPECT_DOUBLE_EQ((*w)(0), 6.0);
+  EXPECT_DOUBLE_EQ((*w)(1), 4.0);
+  delete w;
+}
+
+TEST(MatrixSerialTest, Test_transposeMult_Vector_Vector_reference)
+{
+ /**
+   *  Build matrix [ 1.0   0.0]
+   *               [ 1.0   1.0]
+   *
+   */
+  double asymmetric[4] = {1.0, 0.0, 1.0, 1.0};
+  CAROM::Matrix asymmetric_matrix(asymmetric, 2, 2, false, true);
+
+  /**
+   *  Build vector [ 2.0]
+   *               [ 4.0]
+   */
+  double v_data[2] = {2.0, 4.0};
+  CAROM::Vector v(v_data, 2, false, true);
+
+  /**
+   *  [ 1.0   0.0]^{T} [ 2.0] = [ 6.0]
+   *  [ 1.0   1.0]     [ 4.0]   [ 4.0]
+   *
+   */
+  CAROM::Vector w(2, false);
+  asymmetric_matrix.transposeMult(v, w);
+  EXPECT_FALSE(w.distributed());
+  EXPECT_EQ(w.dim(), 2);
+  EXPECT_DOUBLE_EQ(w(0), 6.0);
+  EXPECT_DOUBLE_EQ(w(1), 4.0);
+}
+
 int main(int argc, char* argv[])
 {
   ::testing::InitGoogleTest(&argc, argv);
