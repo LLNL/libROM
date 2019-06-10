49e3e5c7d64304426904e6e6d812d096cdc89aa7
Robert W. Anderson <anderson110@llnl.gov>
2019-06-10 13:09:45 -0700

    add toss3 intel configure script, enable gtest in both toss3 intel and gnu

--- scripts/configure_toss_3_x86_64_ib_mvapich2-2.3-gcc-4.9.3.sh
+++ scripts/configure_toss_3_x86_64_ib_mvapich2-2.3-gcc-4.9.3.sh
@@ -1,6 +1,18 @@
 #/bin/bash
 
+# choose versions
+
 mvapich="mvapich2-2.3"
 gcc="gcc-4.9.3"
+hdf5=hdf5-serial-1.8.18
 
-./configure --with-CXX=/usr/tce/packages/mvapich2/${mvapich}-${gcc}/bin/mpicxx --with-hdf5=/usr/tce/packages/hdf5/hdf5-serial-1.8.18-${gcc}/bin/h5cc --with-lapack='-L/usr/gapps/bdiv/toss_3_x86_64_ib/gnu-4.9-${mvapich}/lapack/3.5.0 -llapack'
+# derived vars below here
+
+gnu_major=$( echo $gcc | sed 's/gcc/gnu/' | cut -d. -f1-2 )
+gapps=/usr/gapps/bdiv/toss_3_x86_64_ib/${gnu_major}-${mvapich}
+
+./configure \
+  --with-CXX=/usr/tce/packages/mvapich2/${mvapich}-${gcc}/bin/mpicxx \
+  --with-hdf5=/usr/tce/packages/hdf5/hdf5-serial-1.8.18-${gcc}/bin/h5cc \
+  --with-lapack="-L${gapps}/{gnu_major}-${mvapich}/lapack/3.5.0 -llapack" \
+  --with-gtest=${gapps}/gmock/1.7.0/gtest
---
+++ scripts/configure_toss_3_x86_64_ib_mvapich2-2.3-intel-19.0.4.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+# choose versions
+
+mvapich="mvapich2-2.3"
+intel="intel-18.0.1"
+hdf5=hdf5-serial-1.8.18
+
+# derived vars below here
+
+intel_major=$( echo $intel | cut -d. -f1 )
+gapps=/usr/gapps/bdiv/toss_3_x86_64_ib/${intel_major}-${mvapich}
+
+./configure \
+  --with-CXX=/usr/tce/packages/mvapich2/${mvapich}-${intel}/bin/mpicxx \
+  --with-hdf5=/usr/tce/packages/hdf5/${hdf5}-${intel}/bin/h5cc \
+  --with-lapack='-L${gapps}/lapack/3.5.0 -llapack' \
+  --with-gtest=${gapps}/gmock/1.7.0/gtest
