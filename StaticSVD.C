c8583d45a6d281ac6f70b9e58ed00cc745c0d0a8
Sean McBane <sean.mcbane@protonmail.com>
2019-07-11 08:42:36 -0700

    Merge changes from distributed SVD

--- StaticSVD.C
+++ StaticSVD.C
@@ -18,55 +18,70 @@
 #include <stdio.h>
 #include <string.h>
 
-/* Use Autotools-detected Fortran name-mangling scheme */
-#define dgesdd CAROM_FC_GLOBAL(dgesdd, DGESDD)
-
-extern "C" {
-void dgesdd(char*, int*, int*, double*, int*,
-             double*, double*, int*, double*, int*,
-             double*, int*, int*, int*);
-}
-
 namespace CAROM {
-
-const int StaticSVD::COMMUNICATE_A = 999;
 
 StaticSVD::StaticSVD(
    int dim,
    int samples_per_time_interval,
+   int max_basis_dimension,
+   double sigma_tolerance,
    bool debug_algorithm) :
    SVD(dim, samples_per_time_interval, debug_algorithm),
-   d_samples(0),
-   d_V(0),
-   d_this_interval_basis_current(false)
+   d_samples(new SLPK_Matrix), d_factorizer(new SVDManager),
+   d_this_interval_basis_current(false),
+   d_max_basis_dimension(max_basis_dimension),
+   d_sigma_tol(sigma_tolerance)
 {
    CAROM_ASSERT(dim > 0);
    CAROM_ASSERT(samples_per_time_interval > 0);
+   CAROM_ASSERT(max_basis_dimension > 0);
+   CAROM_ASSERT(sigma_tolerance >= 0);
 
    // Get the rank of this process, and the number of processors.
    int mpi_init;
    MPI_Initialized(&mpi_init);
-   if (mpi_init) {
-      MPI_Comm_rank(MPI_COMM_WORLD, &d_rank);
-      MPI_Comm_size(MPI_COMM_WORLD, &d_num_procs);
-   }
-   else {
-      d_rank = 0;
-      d_num_procs = 1;
-   }
+   if (mpi_init == 0) { MPI_Init(nullptr, nullptr); }
+   
+   MPI_Comm_rank(MPI_COMM_WORLD, &d_rank);
+   MPI_Comm_size(MPI_COMM_WORLD, &d_num_procs);
+   
+   get_global_info();
+   /* TODO: Try doing this more intelligently and see if it makes a difference */
+   d_nprow = d_num_procs;
+   d_npcol = 1;
+   d_blocksize = d_total_dim / d_nprow;
+   if (d_total_dim % d_nprow != 0) { d_blocksize += 1; }
 
+   initialize_matrix(d_samples.get(), d_total_dim, d_samples_per_time_interval,
+                     d_nprow, d_npcol, d_blocksize, d_blocksize);
+   d_factorizer->A = nullptr;
 }
 
 StaticSVD::~StaticSVD()
 {
-   // Delete data members.
-   if (d_V) {
-      delete d_V;
+   delete_samples();
+   delete_factorizer();
+}
+
+void StaticSVD::delete_samples()
+{
+   if (d_samples) {
+      free_matrix_data(d_samples.get());
+      release_context(d_samples.get());
    }
-   for (int i = 0; i < static_cast<int>(d_samples.size()); ++i) {
-      if (d_samples[i]) {
-         delete [] d_samples[i];
-      }
+}
+
+void StaticSVD::delete_factorizer()
+{
+   if (d_factorizer->A != nullptr) {
+      free(d_factorizer->S);
+      d_factorizer->S = nullptr;
+      if (d_factorizer->U != nullptr) { free_matrix_data(d_factorizer->U); }
+      free(d_factorizer->U);
+      d_factorizer->U = nullptr;
+      if (d_factorizer->V != nullptr) { free_matrix_data(d_factorizer->V); }
+      free(d_factorizer->V);
+      d_factorizer->V = nullptr;
    }
 }
 
@@ -78,6 +93,7 @@ StaticSVD::takeSample(
 {
    CAROM_ASSERT(u_in != 0);
    CAROM_ASSERT(time >= 0.0);
+   CAROM_NULL_USE(add_without_increase);
 
    // Check the u_in is not non-zero.
    Vector u_vec(u_in, d_dim, true);
@@ -86,37 +102,34 @@ StaticSVD::takeSample(
    }
 
    if (isNewTimeInterval()) {
-
       // We have a new time interval.
+      delete_factorizer();
       int num_time_intervals =
          static_cast<int>(d_time_interval_start_times.size());
       if (num_time_intervals > 0) {
-	 delete d_basis;
-	 d_basis = NULL;
-	 delete d_basis_right;
-	 d_basis_right = NULL;
-         for (int i = 0; i < static_cast<int>(d_samples.size()); ++i) {
-            if (d_samples[i]) {
-               delete [] d_samples[i];
-            }
-         }
-         d_samples.resize(0);
+	      delete d_basis;
+	      d_basis = nullptr;
+	      delete d_basis_right;
+	      d_basis_right = nullptr;
          delete d_U;
-         d_U = NULL;
+         d_U = nullptr;
          delete d_S;
-         d_S = NULL;
-         delete d_V;
-         d_V = NULL;
+         d_S = nullptr;
+         delete d_W;
+         d_W = nullptr;
       }
       d_num_samples = 0;
-      d_time_interval_start_times.resize(num_time_intervals+1);
-      d_time_interval_start_times[num_time_intervals] = time;
-      d_basis = NULL;
-      d_basis_right = NULL;
+      d_time_interval_start_times.resize(
+                static_cast<unsigned>(num_time_intervals) + 1);
+      d_time_interval_start_times[static_cast<unsigned>(num_time_intervals)] =
+          time;
+      d_basis = nullptr;
+      d_basis_right = nullptr;
+      // Set the N in the global matrix so BLACS won't complain.
+      d_samples->n = d_samples_per_time_interval;
    }
-   double* sample = new double [d_dim];
-   memcpy(sample, u_in, d_dim*sizeof(double));
-   d_samples.push_back(sample);
+
+   broadcast_sample(u_in);
    ++d_num_samples;
    d_this_interval_basis_current = false;
    return true;
@@ -128,21 +141,16 @@ StaticSVD::getSpatialBasis()
    // If this basis is for the last time interval then it may not be up to date
    // so recompute it.
    if (!thisIntervalBasisCurrent()) {
-      if (d_basis != 0) {
-         delete d_basis;
-      }
-      if (d_basis_right != 0) {
-         delete d_basis_right;
-      }
-      if (d_U != 0) {
-         delete d_U;
-      }
-      if (d_S != 0) {
-         delete d_S;
-      }
-      if (d_V != 0) {
-         delete d_V;
-      }
+      delete d_basis;
+      d_basis = nullptr;
+      delete d_basis_right;
+      d_basis_right = nullptr;
+      delete d_U;
+      d_U = nullptr;
+      delete d_S;
+      d_S = nullptr;
+      delete d_W;
+      d_W = nullptr;
       computeSVD();
    }
    else {
@@ -159,10 +167,15 @@ StaticSVD::getTemporalBasis()
    // so recompute it.
    if (!thisIntervalBasisCurrent()) {
       delete d_basis;
+      d_basis = nullptr;
       delete d_basis_right;
+      d_basis_right = nullptr;
       delete d_U;
+      d_U = nullptr;
       delete d_S;
-      delete d_V;
+      d_S = nullptr;
+      delete d_W;
+      d_W = nullptr;
       computeSVD();
    }
    else {
@@ -179,10 +192,15 @@ StaticSVD::getSingularValues()
    // be up to date so recompute them.
    if (!thisIntervalBasisCurrent()) {
       delete d_basis;
+      d_basis = nullptr;
       delete d_basis_right;
+      d_basis = nullptr;
       delete d_U;
+      d_U = nullptr;
       delete d_S;
-      delete d_V;
+      d_S = nullptr;
+      delete d_W;
+      d_W = nullptr;
       computeSVD();
    }
    else {
@@ -195,237 +213,103 @@ StaticSVD::getSingularValues()
 void
 StaticSVD::computeSVD()
 {
-   // Get the dimensions from each process and the total dimension.
-   int* dims = new int [d_num_procs];
-   if (d_num_procs > 1) {
-      MPI_Allgather(&d_dim, 1, MPI_INT, dims, 1, MPI_INT, MPI_COMM_WORLD);
-   }
-   else {
-      dims[0] = d_dim;
-   }
+   // This block does the actual ScaLAPACK call to do the factorization.
+   d_samples->n = d_num_samples;
+   delete_factorizer();
+   svd_init(d_factorizer.get(), d_samples.get());
+   d_factorizer->dov = 1;
+   factorize(d_factorizer.get());
 
-   int total_dim = 0;
-   for (int i = 0; i < d_num_procs; ++i) {
-      total_dim += dims[i];
-   }
-
-   // Do this computation on process 0 and broadcast results to the other
-   // processes.
-   int num_cols = static_cast<int>(d_samples.size());
-   if (d_rank == 0) {
-      // Construct storage for the globalized A.
-      double* A = new double [total_dim*num_cols];
-
-      // Put this processor's contribution to A into it in column major order.
-      int idx = 0;
-      for (int col = 0; col < num_cols; ++col) {
-         double* col_vals = d_samples[col];
-         for (int row = 0; row < d_dim; ++row) {
-            A[idx+row] = col_vals[row];
-         }
-         idx += total_dim;
-      }
-
-      // Get the contributions to the globalized A from the other processes.
-      if (d_num_procs > 1) {
-         int offset = dims[0];
-         for (int proc = 1; proc < d_num_procs; ++proc) {
-            int this_proc_dim = dims[proc];
-            double* Aproc = new double [this_proc_dim*num_cols];
-            MPI_Status status;
-            MPI_Recv(Aproc,
-               this_proc_dim*num_cols,
-               MPI_DOUBLE,
-               proc,
-               COMMUNICATE_A,
-               MPI_COMM_WORLD,
-               &status);
-            int Aproc_idx = 0;
-            int Aidx = offset;
-            for (int col = 0; col < num_cols; ++col) {
-               for (int row = 0; row < this_proc_dim; ++row) {
-                  A[Aidx+row] = Aproc[Aproc_idx++];
-               }
-               Aidx += total_dim;
-            }
-            delete [] Aproc;
-            offset += this_proc_dim;
+   // Compute how many basis vectors we will actually return.
+   int sigma_cutoff = 0, hard_cutoff = d_num_samples;
+   if (d_sigma_tol == 0) { 
+      sigma_cutoff = std::numeric_limits<int>::max();
+   } else {
+      for (int i = 0; i < d_num_samples; ++i) {
+         if (d_factorizer->S[i] / d_factorizer->S[0] > d_sigma_tol) {
+            sigma_cutoff += 1;
+         } else {
+            break;
          }
       }
-
-      // Perform the svd.
-      svd(A, total_dim);
-
-      // Broadcast the results.
-      if (d_num_procs > 1) {
-         MPI_Bcast(&d_U->item(0, 0),
-            total_dim*num_cols,
-            MPI_DOUBLE,
-            0,
-            MPI_COMM_WORLD);
-         MPI_Bcast(&d_S->item(0, 0),
-            num_cols*num_cols,
-            MPI_DOUBLE,
-            0,
-            MPI_COMM_WORLD);
-         MPI_Bcast(&d_V->item(0, 0),
-            num_cols*num_cols,
-            MPI_DOUBLE,
-            0,
-            MPI_COMM_WORLD);
-      }
-
-      // Clean up.
-      delete [] A;
    }
-   else {
-      // Put this processor's contribution to the globalized of A into A in
-      // column major order.
-      double* A = new double [d_dim*num_cols];
-      int idx = 0;
-      for (int col = 0; col < num_cols; ++col) {
-         double* col_vals = d_samples[col];
-         for (int row = 0; row < d_dim; ++row) {
-            A[idx++] = col_vals[row];
-         }
-      }
-
-      // Send the contribution to the globalized A to process 0.
-      MPI_Request request;
-      MPI_Isend(A,
-         d_dim*num_cols,
-         MPI_DOUBLE,
-         0,
-         COMMUNICATE_A,
-         MPI_COMM_WORLD,
-         &request);
-
-      // Allocate d_U, d_S, and d_V.
-      d_U = new Matrix(total_dim, num_cols, false);
-      d_S = new Matrix(num_cols, num_cols, false);
-      d_V = new Matrix(num_cols, num_cols, false);
-
-      // Get the results from process 0.
-      MPI_Bcast(&d_U->item(0, 0),
-         total_dim*num_cols,
-         MPI_DOUBLE,
-         0,
-         MPI_COMM_WORLD);
-      MPI_Bcast(&d_S->item(0, 0),
-         num_cols*num_cols,
-         MPI_DOUBLE,
-         0,
-         MPI_COMM_WORLD);
-      MPI_Bcast(&d_V->item(0, 0),
-         num_cols*num_cols,
-         MPI_DOUBLE,
-         0,
-         MPI_COMM_WORLD);
-
-      // Clean up.
-      delete [] A;
+   if (d_max_basis_dimension < hard_cutoff) {
+      hard_cutoff = d_max_basis_dimension;
    }
-   d_basis = new Matrix(*d_U);
-   d_basis_right = new Matrix(*d_V);
+   int ncolumns = hard_cutoff < sigma_cutoff ? hard_cutoff : sigma_cutoff;
+   CAROM_ASSERT(ncolumns >= 0);
+
+   // Allocate the appropriate matrices and gather their elements.
+   d_basis = new Matrix(d_dim, ncolumns, true);
+   d_S = new Matrix(ncolumns, ncolumns, false);
+   {
+      CAROM_ASSERT(ncolumns >= 0);
+      unsigned nc = static_cast<unsigned>(ncolumns);
+      memset(&d_S->item(0, 0), 0, nc*nc*sizeof(double));
+   }
+   
+   d_basis_right = new Matrix(ncolumns, d_num_samples, false);
+   for (int rank = 0; rank < d_num_procs; ++rank) {
+      // gather_transposed_block does the same as gather_block, but transposes
+      // it; here, it is used to go from column-major to row-major order.
+      gather_transposed_block(&d_basis->item(0, 0), d_factorizer->U,
+                              d_istarts[static_cast<unsigned>(rank)]+1,
+                              1, d_dims[static_cast<unsigned>(rank)],
+                              ncolumns, rank);
+      // V is computed in the transposed order so no reordering necessary.
+      gather_block(&d_basis_right->item(0, 0), d_factorizer->V, 1, 1,
+                   ncolumns, d_num_samples, rank);
+   }
+   for (int i = 0; i < ncolumns; ++i)
+      d_S->item(i, i) = d_factorizer->S[static_cast<unsigned>(i)];
    d_this_interval_basis_current = true;
-   if (d_debug_algorithm && d_rank == 0) {
-      for (int row = 0; row < num_cols; ++row) {
-         for (int col = 0; col < num_cols; ++col) {
-            printf("%.16e ", d_S->item(row, col));
-         }
+
+   if (d_debug_algorithm) {
+      if (d_rank == 0) {
+         printf("Distribution of sampler's A and U:\n");
+      }
+      print_debug_info(d_samples.get());
+      MPI_Barrier(MPI_COMM_WORLD);
+
+      if (d_rank == 0) {
+         printf("Distribution of sampler's V:\n");
+      }
+      print_debug_info(d_factorizer->V);
+      MPI_Barrier(MPI_COMM_WORLD);
+
+      if (d_rank == 0) {
+         printf("Computed singular values: ");
+         for (int i = 0; i < ncolumns; ++i)
+            printf("%8.4E  ", d_factorizer->S[i]);
          printf("\n");
       }
-      printf("\n");
-      for (int row = 0; row < num_cols; ++row) {
-         for (int col = 0; col < num_cols; ++col) {
-            printf("%.16e ", d_V->item(row, col));
-         }
-         printf("\n");
-      }
-      printf("\n");
-      for (int row = 0; row < total_dim; ++row) {
-         for (int col = 0; col < num_cols; ++col) {
-            printf("%.16e ", d_U->item(row, col));
-         }
-         printf("\n");
-      }
-      printf("============================================================\n");
    }
-   delete [] dims;
 }
 
 void
-StaticSVD::svd(
-   double* A,
-   int total_dim)
+StaticSVD::broadcast_sample(const double* u_in)
 {
-   CAROM_ASSERT(A != 0);
-   CAROM_ASSERT(total_dim > 0);
+   for (int rank = 0; rank < d_num_procs; ++rank) {
+      scatter_block(d_samples.get(), d_istarts[static_cast<unsigned>(rank)]+1,
+                    d_num_samples+1, u_in, d_dims[static_cast<unsigned>(rank)],
+                    1, rank);
+   }
+}
 
-   int num_samples = static_cast<int>(d_samples.size());
-
-   // Construct d_U.
-   d_U = new Matrix(total_dim, num_samples, false);
-
-   // Construct d_S.
-   d_S = new Matrix(num_samples, num_samples, false);
-   for (int row = 0; row < num_samples; ++row) {
-      for (int col = 0; col < num_samples; ++col) {
-         d_S->item(row, col) = 0.0;
+void
+StaticSVD::get_global_info()
+{
+   d_dims.resize(static_cast<unsigned>(d_num_procs));
+   MPI_Allgather(&d_dim, 1, MPI_INT, d_dims.data(), 1, MPI_INT, MPI_COMM_WORLD);
+   d_total_dim = 0;
+   d_istarts = std::vector<int>(static_cast<unsigned>(d_num_procs), 0);
+   
+   for (unsigned i = 0; i < static_cast<unsigned>(d_num_procs); ++i) {
+      d_total_dim += d_dims[i];
+      if (i > 0) {
+         d_istarts[i] = d_istarts[i-1] + d_dims[i-1];
       }
    }
-
-   // Construct d_V.
-   d_V = new Matrix(num_samples, num_samples, false);
-
-   // Use lapack's dgesdd Fortran function to perform the svd.  As this is
-   // Fortran A and all the computed matrices are in column major order.
-   char jobz = 'A';
-   int m = total_dim;
-   int n = num_samples;
-   int lda = m;
-   double* sigma = new double [num_samples];
-   double* U = new double[m*m];
-   int ldu = m;
-   int ldv = num_samples;
-   int lwork = n*(4*n + 6)+m;
-   double* work = new double [lwork];
-   int* iwork = new int [8*n];
-   int info;
-   dgesdd(&jobz,
-	  &m,
-	  &n,
-	  A,
-	  &lda,
-	  sigma,
-	  U,
-	  &ldu,
-	  &d_V->item(0, 0),
-	  &ldv,
-	  work,
-	  &lwork,
-	  iwork,
-	  &info);
-   CAROM_ASSERT(info == 0);
-   delete [] work;
-   delete [] iwork;
-
-   // Place sigma into d_S.
-   for (int i = 0; i < num_samples; ++i) {
-      d_S->item(i, i) = sigma[i];
-   }
-   delete [] sigma;
-
-   // Take the first n rows of U and place into d_U.  U is in column major
-   // order so convert is to row major order as this is done.
-   int uidx = 0;
-   for (int row = 0; row < n; ++row) {
-      for (int col = 0; col < m; ++col) {
-         d_U->item(col, row) = U[uidx++];
-      }
-   }
-   delete [] U;
-
 }
 
 }
