794b529eb992a2b27fb467484059e5e5a7e2107a
Jessie Lauzon <jessicatlauzon@gmail.com>
2019-07-30 18:19:21 -0700

    add getSnapshotMatrix function

--- StaticSVD.h
+++ StaticSVD.h
@@ -117,6 +117,15 @@ class StaticSVD : public SVD
       const Matrix*
       getSingularValues();
 
+      /**
+       * @brief Returns the snapshot  matric for the current time interval.
+       *
+       * @return The snapshot matrix for the current time interval.
+       */
+      virtual
+      double*
+      StaticSVD::getSnapshotMatrix()
+   
    private:
       /**
        * @brief Unimplemented default constructor.
