7e96499738478718ec010c09a6fb716908b6bf6e
Geoffrey M Oxberry <goxberry@gmail.com>
2019-12-18 01:48:40 -0800

    CMake: update TOSS3 toolchain file

--- cmake/toolchains/ic18-toss_3_x86_64_ib-librom-dev.cmake
+++ cmake/toolchains/ic18-toss_3_x86_64_ib-librom-dev.cmake
@@ -13,6 +13,10 @@ set(BLA_VENDOR Intel10_64lp)
 set(HDF5_ROOT /usr/tce/packages/hdf5/hdf5-serial-1.8.18-intel-18.0.1)
 #string(APPEND CMAKE_CXX_FLAGS_INIT " -gxx-name=/usr/tce/packages/gcc/gcc-4.9.3/bin")
 
+set(BLAS_ROOT /usr/tce/packages/mkl/mkl-2019.0/mkl)
+set(LAPACK_ROOT /usr/tce/packages/mkl/mkl-2019.0/mkl)
+set(CMAKE_LIBRARY_ARCHITECTURE intel64)
+
 set(MKL_MESSAGE_PASSING MPICH2)
 set(MKL_THREADING Sequential)
-set(MKL_ROOT /usr/gce/packages/mkl/mkl-2019.0/mkl)
+set(MKL_ROOT /usr/tce/packages/mkl/mkl-2019.0/mkl)
