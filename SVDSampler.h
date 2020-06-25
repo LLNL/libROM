fdd1c9182e4b4a179052500463ba240154fba9cb
Dylan Copeland <copeland11@llnl.gov>
2020-06-24 18:11:06 -0700

    Fixed a parallel bug in DEIM. Fixed some memory leaks in Matrix.C. Added functions to access the number of samples taken in a basis generator.

--- SVDSampler.h
+++ SVDSampler.h
@@ -201,6 +201,11 @@ class SVDSampler
          return d_updateRightSV;
       }
 
+      int getNumSamples() const
+      {
+	return d_svd->getNumSamples();
+      }
+      
    protected:
       /**
        * @brief Pointer to the abstract SVD algorithm object.
