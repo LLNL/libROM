89c812f464b4a713d08077870045c6ffe117ddcc
Kevin Huy-Khoa Huynh <huynh24@llnl.gov>
2020-10-28 14:06:49 -0700

    fix deim matlab script to handle m x n matrices

--- matlab/deim.m
+++ matlab/deim.m
@@ -1,14 +1,16 @@
-function [inv_U] = deim(Q)
+function [inv_Q] = deim(Q)
 % Input: Q: n by m matrix with orthonormal columns
 
     phi = [];
     [rho, phi(end + 1)] = max(abs(Q(:,1)));
     U = Q(:,1);
-    P = zeros(m,1);
+    m = size(Q,2);
+    n = size(Q,1);
+    P = zeros(n,1);
     P(phi(end)) = 1;
     Q_sampled = Q(phi(end),:);
     for l = 2:m
-        M = transpose(P)*U;ls
+        M = transpose(P)*U;
         
         inv_M = inv(M);
         RHS = transpose(P) * Q(:, l);
@@ -16,11 +18,12 @@ function [inv_U] = deim(Q)
         r = Q(:,l) - U*c;
         [rho, phi(end + 1)] = max(abs(r));
         U = [U Q(:,l)];
-        newPcol = zeros(m,1);
+        newPcol = zeros(n,1);
         newPcol(phi(end)) = 1;
         P = [P newPcol];
         Q_sampled = [Q_sampled;Q(phi(end),:)];
     end
-    phi = transpose(phi);
-    inv_U = inv(U);
+    [phi, phi_sort_order] = sort(phi);
+    Q_sampled = Q_sampled(phi_sort_order,:);
+    inv_Q = inv(Q_sampled);
 end
