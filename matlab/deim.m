cffaec0da60ceefcc583896ed52993ab6690b696
Kevin Huy-Khoa Huynh <huynh24@llnl.gov>
2020-10-28 10:29:44 -0700

    add matlab deim script

--- matlab/deim.m
+++ matlab/deim.m
@@ -1,21 +1,26 @@
-m =  5
-n = 25
-A = randn(m, n)
-[Q, R] = qr(A)
-phi = []
-[rho, phi(end + 1)] = max(Q(:,1))
-U = Q(:,1)
-P = zeros(m,1)
-P(phi(end)) = 1
-Q_sampled = Q(phi(end),:)
-for l = 2:m
-    c = inv(transpose(P)*U)*transpose(P)*Q(:,l)
-    r = Q(:,l) - U*c
-    [rho, phi(end + 1)] = max(r)
-    U = [U Q(:,l)]
-    newPcol = zeros(m,1)
-    newPcol(phi(end)) = 1
-    P = [P newPcol]
-    Q_sampled = [Q_sampled;Q(phi(end),:)]
+function [inv_U] = deim(Q)
+% Input: Q: n by m matrix with orthonormal columns
+
+    phi = [];
+    [rho, phi(end + 1)] = max(abs(Q(:,1)));
+    U = Q(:,1);
+    P = zeros(m,1);
+    P(phi(end)) = 1;
+    Q_sampled = Q(phi(end),:);
+    for l = 2:m
+        M = transpose(P)*U;ls
+        
+        inv_M = inv(M);
+        RHS = transpose(P) * Q(:, l);
+        c = inv_M * RHS;
+        r = Q(:,l) - U*c;
+        [rho, phi(end + 1)] = max(abs(r));
+        U = [U Q(:,l)];
+        newPcol = zeros(m,1);
+        newPcol(phi(end)) = 1;
+        P = [P newPcol];
+        Q_sampled = [Q_sampled;Q(phi(end),:)];
+    end
+    phi = transpose(phi);
+    inv_U = inv(U);
 end
-phi = transpose(phi)
