2e323c7e70f74da06ee9e63a55f25f34b3734f67
Kevin Huy-Khoa Huynh <huynh24@llnl.gov>
2020-10-29 16:01:41 -0700

    rename gpode to qdeim

--- matlab/qdeim.m
+++ matlab/qdeim.m
@@ -1,15 +1,27 @@
-function [S, M] = qdeim(U)
-% Input:  U: n by m matrix with orthonormal columns
-% Output: S: selection of m row indices with guaranteed upper bound
-%            norm(inv(U(S,:))) <= sqrt(n - m + 1) * O(2 ^ m)
-%         M: the matrix U * inv(U(S, :));
-%            The Q-DEIM projection of an n-by-1 vector f is M*f(S)
-    [n, m] = size(U);
-    if nargout == 1
-        [~, ~, P] = qr(U', 'vector'); S = P(1:m);
+function [inv_U] = gpode(U, m)
+    [~, ~, p] = qr(U', 'vector');
+    p = p(1:size(U, 2))';
+    for i = length(p) + 1:m
+        [~, S, W] = svd(U(p, :), 0);
+        g = S(end - 1, end - 1).^2 - S(end, end)^2;
+        Ub = W'*U';
+        r = g + sum(Ub.^2,1);
+        r = r - sqrt((g+sum(Ub.^2, 1)).^2 - 4 * g * Ub(end, :).^2);
+        [~, I] = sort(r, 'descend');
+        e = 1;
+        while any(I(e) == p)
+            e = e + 1;
+        end
+        p(end + 1) = I(e);
+    end
+    [p, ~] = sort(p);
+    U_sampled = [];
+    for i = 1:size(p,1)
+        U_sampled = [U_sampled; U(p(i,1),:)];
+    end
+    if m == size(U,2)
+        inv_U = pinv(U_sampled);
     else
-        [Q, R, P] = qr(U', 'vector'); S = P(1:m);
-        M = [eye(m); (R(:, 1:m) \ R(:, (m + 1):n))'];
-        Pinverse(P) = 1:n; M = M(Pinverse,:);
+        inv_U = transpose(pinv(U_sampled));
     end
 end
