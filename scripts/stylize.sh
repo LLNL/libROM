9dd89f15cc140eda5b9b40e24eee9b5b058292e3
rollbackcc <kevinhkhuynh@gmail.com>
2021-04-14 11:10:01 -0700

    add style script

---
+++ scripts/stylize.sh
@@ -0,0 +1,9 @@
+if [ "$1" == "" ] || [ $# -gt 1 ]; then
+    echo "Usage: ./stylize.sh /path/to/astyle/executable"
+    exit 0
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(dirname "$DIR")"
+
+$1 $DIR/*.C $DIR/*.h $DIR/*.c $DIR/*.f90 $DIR/*h.in $DIR/tests/*.C
