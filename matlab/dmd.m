261668db4e3f71539b360fe251837f718c089b72
Kevin Huy-Khoa Huynh <huynh24@llnl.gov>
2021-08-02 11:03:59 -0700

    update matlab implementation

--- matlab/dmd.m
+++ matlab/dmd.m
@@ -1,6 +1,8 @@
-function dmd(X, m_used)
+function dmd(X, m_used, t, dt)
 % Input:      X: n by m snapshot matrix
 %        m_used: number of basis_vectors to use
+%             t: The time to predict
+%            dt: delta time
 
     X1 = X(:,1:end-1);
     X2 = X(:,2:end);
@@ -8,17 +10,12 @@ function dmd(X, m_used)
 
     [U, S, V] = svd(X1, 'econ');
     U = U(:,1:m_used);
-    U(1:5,1:5);
-    V(1:5,1:5);
-    S;
-    inv(S)
     S = S(1:m_used,1:m_used);
     V = V(:,1:m_used);
     Atilde = U'*X2*V*inv(S);
     [W,eigs] = eig(Atilde);
-    eigs;
     Phi = X2*V*inv(S)*W;
-    v = Phi*eigs^100*pinv(Phi)*b0;
-    norm(real(v-X(:,end)))/2340
+    pred = Phi*eigs^(t/dt)*pinv(Phi)*b0;
+    norm(real(pred-X(:,end)))/norm(real(X(:,end)))
 
 end
