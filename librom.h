306a4ff6332aca414548455fa914623084434bce
Kevin Huy-Khoa Huynh <huynh24@llnl.gov>
2021-10-29 12:00:50 -0700

    add librom.h

---
+++ librom.h
@@ -0,0 +1,31 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2013-2021, Lawrence Livermore National Security, LLC
+ * and other libROM project developers. See the top-level COPYRIGHT
+ * file for details.
+ *
+ * SPDX-License-Identifier: (Apache-2.0 OR MIT)
+ *
+ *****************************************************************************/
+
+#ifndef included_librom_h
+#define included_librom_h
+
+#include "linalg/BasisGenerator.h"
+#include "linalg/BasisReader.h"
+#include "linalg/Options.h"
+#include "linalg/Matrix.h"
+#include "linalg/Vector.h"
+#include "algo/DMD.h"
+#include "algo/greedy/GreedyCustomSampler.h"
+#include "algo/greedy/GreedyRandomSampler.h"
+#include "algo/manifold_interp/MatrixInterpolater.h"
+#include "algo/manifold_interp/VectorInterpolater.h"
+#include "hyperreduction/DEIM.h"
+#include "hyperreduction/QDEIM.h"
+#include "hyperreduction/STSampling.h"
+#ifdef USEMFEM
+#include "mfem/SampleMesh.hpp"
+#endif
+
+#endif
