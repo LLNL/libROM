df2fcf67bb4967006ad51139d7c825b47b8b5a52
rollbackcc <kevinhkhuynh@gmail.com>
2021-11-12 10:40:16 -0800

    change softwareupate

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -74,7 +74,7 @@ if [ "$(uname)" == "Darwin" ]; then
   if [[ $? != 0 ]] ; then
       xcode-select --install
   fi
-  softwareupdate -r -i
+  softwareupdate --all --install --force
   brew list open-mpi > /dev/null || brew install open-mpi
   brew list openblas > /dev/null || brew install openblas
   brew list lapack > /dev/null || brew install lapack
