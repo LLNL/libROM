cf4c42950fde9cf143205afb0284112b04d1aff0
Cheung <cheung26@kidamasaomi.llnl.gov>
2022-06-09 14:33:48 -0700

    Update to year 2022

--- tests/test_DMD.C
+++ tests/test_DMD.C
@@ -2,7 +2,7 @@
 
 /******************************************************************************
  *
- * Copyright (c) 2013-2021, Lawrence Livermore National Security, LLC
+ * Copyright (c) 2013-2022, Lawrence Livermore National Security, LLC
  * and other libROM project developers. See the top-level COPYRIGHT
  * file for details.
  *
