552e71c4f7d82ee022ed4384a37d3c6e6e967f05
siuwuncheung <61989266+siuwuncheung@users.noreply.github.com>
2022-06-09 07:02:53 -0700

    Update description

--- lib/algo/NonuniformDMD.h
+++ lib/algo/NonuniformDMD.h
@@ -8,10 +8,13 @@
  *
  *****************************************************************************/
 
-// Description: Computes the DMD algorithm on the given snapshot matrix. The
-//              implemented dynamic mode decomposition algorithm is derived from
+// Description: Computes the DMD algorithm on the given snapshot matrix 
+//              with non-uniform sampling time steps. 
+//              The implemented dynamic mode decomposition algorithm is modified from
 //              Tu et. al's paper "On Dynamic Mode Decomposition: Theory and
 //              Applications": https://arxiv.org/abs/1312.0041
+//              Instead of approximating the discrete dynamics, this algorithm 
+//              approximates the continuous dynamics linearly. 
 //              This algorithm also works in the case that the first sample does
 //              not start from t = 0.0 by incorporating a time offset.
 
