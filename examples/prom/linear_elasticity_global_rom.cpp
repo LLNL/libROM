90fbd1a7176a57c7c081151a4aa085591f7aec26
Axel Larsson <iamaxellarsson@gmail.com>
2022-07-01 11:15:12 +0200

    Started Linear Elastic example

---
+++ examples/prom/linear_elasticity_global_rom.cpp
@@ -0,0 +1,34 @@
+//               libROM MFEM Example: parametric ROM for linear elastic problem (adapted from ex2p.cpp)
+//
+// Compile with: ./scripts/compile.sh -m
+//
+// Description:  This example code demonstrates the use of MFEM and libROM to
+//               define a simple projection-based reduced order model of a 
+//				 simple linear elasticity problem describing a multi-material 
+//				 cantilever beam.
+//
+//               The example highlights three distinct ROM processes, i.e.,
+//               offline, merge, and online. The offline phase runs the full
+//               order model and stores the snapshot data in an HDF file. You
+//               can run as many offline phases as you wish to sample the
+//               parameter space. The merge phase reads all the snapshot files,
+//               builds a global reduced basis, and stores the basis in an HDF
+//               file. The online phase reads the basis, builds the ROM
+//               operator, solves the reduced order system, and lifts the
+//               solution to the full order space.
+//
+// Offline phase: 
+// 
+//
+// Merge phase:   
+//
+// Online phase:  
+
+#include "mfem.hpp"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+using namespace mfem;
+
+cout << "done loading mfem" << endl;
