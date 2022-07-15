5afeb7ffca7ae5bb83cc575fa22a30f8065871ed
Kevin <huynh24@llnl.gov>
2022-07-15 09:21:35 -0700

    move parmetis tar here

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -39,7 +39,6 @@ fi
 cd $LIB_DIR
 if [ ! -d "parmetis-4.0.3" ]; then
 
-  wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.3.tar.gz
   tar -zxvf parmetis-4.0.3.tar.gz
   cd parmetis-4.0.3
   make config
