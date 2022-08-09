aa8c86eeeeb728fba20e88ca76b9c22a9871852e
Axel Larsson <65452706+axla-io@users.noreply.github.com>
2022-08-09 18:10:39 +0200

    new small fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -38,6 +38,7 @@ fi
 # Install PARMETIS 4.0.3
 cd $LIB_DIR
 if [ ! -d "parmetis-4.0.3" ]; then
+
   wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.3.tar.gz
   tar -zxvf parmetis-4.0.3.tar.gz
   cd parmetis-4.0.3
