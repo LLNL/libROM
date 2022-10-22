f2e9d144445d593c9ee85f9d8951a31182657ae6
pvempati <pvempati@ucsc.edu>
2022-10-21 20:39:04 -0700

    Added copyrights and removed simulationLogFile

--- regression_tests/checkError.cpp
+++ regression_tests/checkError.cpp
@@ -1,3 +1,12 @@
+/******************************************************************************
+ *
+ * Copyright (c) 2013-2022, Lawrence Livermore National Security, LLC
+ * and other libROM project developers. See the top-level COPYRIGHT
+ * file for details.
+ *
+ * SPDX-License-Identifier: (Apache-2.0 OR MIT)
+ *
+ *****************************************************************************/
 
 #include <string>
 #include <cmath>
