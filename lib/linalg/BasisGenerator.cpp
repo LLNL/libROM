d6b0801bb44549dac62d2b7b58cea3b3ec2c7d4c
Kevin" Seung Whan Chung <dreamer2368@gmail.com>
2023-04-11 17:46:01 -0700

    Print out error message if trivial solution is taken. (#200)
    * print out error message if trivial solution is taken.
    
    * error message revised to show time.

--- lib/linalg/BasisGenerator.cpp
+++ lib/linalg/BasisGenerator.cpp
@@ -132,6 +132,7 @@ BasisGenerator::takeSample(
     // Check that u_in is not non-zero.
     Vector u_vec(u_in, getDim(), true);
     if (u_vec.norm() == 0.0) {
+        printf("WARNING: BasisGenerator::takeSample skipped trivial sample at time %.4E\n", time);
         return false;
     }
 
