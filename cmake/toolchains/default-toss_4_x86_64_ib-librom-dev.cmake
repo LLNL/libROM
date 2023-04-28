4d1e6794c583d048ecb57dd7e79d93f28af1259e
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2023-04-27 17:32:01 -0700

    Building libROM for TOSS4 (#202)
    * added TOSS4 (LC-Quartz) toolchain and a temporary (hacky) fix for MKL version detection
    * proper fix for an arbitrary amount of whitespace between define vars and values
    * added specific TOSS4 toolchains for GNU and Intel compilers
    * changed default toolchain to TOSS4

---
+++ cmake/toolchains/default-toss_4_x86_64_ib-librom-dev.cmake
@@ -0,0 +1,31 @@
+# Toolchain for Lawrence Livermore National Laboratory TOSS4 machines
+# (e.g., Quartz), using the compilers and HDF5 library as available
+# in the environment (i.e., through "module load").
+#
+# Note that:
+#   which mpicc
+#   which mpicxx
+#   which mpif90
+#   which h5diff
+# should point to MPI compilers and the HDF5 library built with
+# the same C/FORTRAN compilers.
+
+# Use MPI compiler wrappers because it simplifies detection of MPI
+set(CMAKE_C_COMPILER mpicc)
+set(CMAKE_CXX_COMPILER mpicxx)
+set(CMAKE_Fortran_COMPILER mpif90)
+set(BLA_VENDOR Intel10_64lp)
+
+execute_process(
+  COMMAND "echo $(which h5diff) | sed -e s/'/bin/h5diff'//"
+  OUTPUT_VARIABLE hdf5_serial_path
+)
+set(HDF5_ROOT ${hdf5_serial_path})
+
+set(BLAS_ROOT /usr/tce/packages/mkl/mkl-2022.1.0)
+set(LAPACK_ROOT /usr/tce/packages/mkl/mkl-2022.1.0)
+set(CMAKE_LIBRARY_ARCHITECTURE intel64)
+
+set(MKL_MESSAGE_PASSING MPICH2)
+set(MKL_THREADING Sequential)
+set(MKL_ROOT /usr/tce/packages/mkl/mkl-2022.1.0)
