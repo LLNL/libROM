9cb4d4cc66caa717a468912fade81b277175cbc9
siuwuncheung <cheung26@llnl.gov>
2023-05-19 09:50:02 -0700

    Add regression test for mixed nonlinear diffusion EQP

--- regression_tests/tests/prom/mixed_nonlinear_diffusion.sh
+++ regression_tests/tests/prom/mixed_nonlinear_diffusion.sh
@@ -4,6 +4,7 @@ CMDS=(
     "./mixed_nonlinear_diffusion -offline -tf 0.01"
     "./mixed_nonlinear_diffusion -merge -ns 1 -tf 0.01"
     "./mixed_nonlinear_diffusion -online -tf 0.01"
+    "./mixed_nonlinear_diffusion -online -tf 0.01 -rrdim 2 -rwdim 2 -nldim 3 -ns 1 -eqp -maxnnls 4"
 )
 TYPE="PROM"
 OFFSET=5
