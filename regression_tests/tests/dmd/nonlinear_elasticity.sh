2a5a9249299372678a8343a2de981597662f34f4
Siu Wun Cheung <cheung26@llnl.gov>
2023-05-24 06:58:04 -0700

    Remove redundant lines

--- regression_tests/tests/dmd/nonlinear_elasticity.sh
+++ regression_tests/tests/dmd/nonlinear_elasticity.sh
@@ -2,19 +2,9 @@
 set -eo pipefail
 source $GITHUB_WORKSPACE/regression_tests/common.sh
 
-
 CMDS=( 
     "$COMMAND ./nonlinear_elasticity -s 2 -rs 1 -dt 0.01 -tf 5" 
 )
 TYPE="DMD"
 OFFSET=5
 run_tests
-
-
-
-
-
-
-
-
-
