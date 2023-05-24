e767302a65f6b1970d4b6ab8928c6a05757b1ba5
Siu Wun Cheung <cheung26@llnl.gov>
2023-05-24 06:57:06 -0700

    Replace reproductive case by parametric case in local PROM for DG advection

--- regression_tests/tests/prom/dg_advection_local_rom_matrix_interp.sh
+++ regression_tests/tests/prom/dg_advection_local_rom_matrix_interp.sh
@@ -5,6 +5,12 @@ CMDS=(
     "rm -rf frequencies.txt" 
     "./dg_advection_local_rom_matrix_interp -offline"
     "./dg_advection_local_rom_matrix_interp -online"
+    "./dg_advection_local_rom_matrix_interp --mesh ../data/periodic-square.mesh -offline -rs 4 -ff 1.02"
+    "./dg_advection_local_rom_matrix_interp --mesh ../data/periodic-square.mesh-interp_prep -rs 4 -ff 1.02 -rdim 40"
+    "./dg_advection_local_rom_matrix_interp --mesh ../data/periodic-square.mesh -offline -rs 4 -ff 1.08"
+    "./dg_advection_local_rom_matrix_interp --mesh ../data/periodic-square.mesh -interp_prep -rs 4 -ff 1.08 -rdim 40"
+    "./dg_advection_local_rom_matrix_interp --mesh ../data/periodic-square.mesh -fom -rs 4 -ff 1.05 -visit"
+    "./dg_advection_local_rom_matrix_interp --mesh ../data/periodic-square.mesh -online_interp -rs 4 -ff 1.05 -rdim 40"
 )
 TYPE="PROM"
 OFFSET=0
