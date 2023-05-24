52ec5ca587e4429dcf28338af656ca002a56b1b7
Siu Wun Cheung <cheung26@llnl.gov>
2023-05-23 21:33:57 -0700

    Fix wrong testing parameter

--- regression_tests/tests/prom/dg_advection_global_rom.sh
+++ regression_tests/tests/prom/dg_advection_global_rom.sh
@@ -7,7 +7,7 @@ CMDS=(
     "./dg_advection_global_rom -offline -ff 1.2 -id 2" 
     "./dg_advection_global_rom -merge -ns 3" 
     "./dg_advection_global_rom -fom -ff 1.15"
-    "./dg_advection_global_rom -online -ff 1.2"
+    "./dg_advection_global_rom -online -ff 1.15"
 )
 TYPE="PROM"
 OFFSET=0
