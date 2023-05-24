90c72c7bc7e7ecf5b25b20ebbb418bb19ddae6d5
Siu Wun Cheung <cheung26@llnl.gov>
2023-05-24 12:02:43 -0700

    Fix regression tests (#214)
    * Add regression test for mixed nonlinear diffusion EQP
    
    * Add SOPT regression test
    
    * Fix command line options for the SOPT regreesion test
    
    * Fix wrong testing parameter
    
    * Replace reproductive case by parametric case in local PROM for DG advection
    
    * Remove redundant lines
    
    * Remove redundant lines
    
    * Modify DG advection local ROM regression tests
    
    * Change output filename
    
    * Add timer to global ROM for DG advection
    
    * Fix timer

--- regression_tests/tests/prom/dg_advection_local_rom_matrix_interp.sh
+++ regression_tests/tests/prom/dg_advection_local_rom_matrix_interp.sh
@@ -3,8 +3,14 @@ source $GITHUB_WORKSPACE/regression_tests/common.sh
 
 CMDS=( 
     "rm -rf frequencies.txt" 
-    "./dg_advection_local_rom_matrix_interp -offline"
-    "./dg_advection_local_rom_matrix_interp -online"
+    "./dg_advection_local_rom_matrix_interp -offline -ff 1.0"
+    "./dg_advection_local_rom_matrix_interp -interp_prep -ff 1.0 -rdim 40"
+    "./dg_advection_local_rom_matrix_interp -offline -ff 1.1"
+    "./dg_advection_local_rom_matrix_interp -interp_prep -ff 1.1 -rdim 40"
+    "./dg_advection_local_rom_matrix_interp -offline -ff 1.2"
+    "./dg_advection_local_rom_matrix_interp -interp_prep -ff 1.2 -rdim 40"
+    "./dg_advection_local_rom_matrix_interp -fom -ff 1.15"
+    "./dg_advection_local_rom_matrix_interp -online_interp -ff 1.15 -rdim 40"
 )
 TYPE="PROM"
 OFFSET=0
