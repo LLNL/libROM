521139152309bb0be1c825ecf0cc99a72934d66b
Siu Wun Cheung <cheung26@llnl.gov>
2023-05-24 07:50:08 -0700

    Modify DG advection local ROM regression tests

--- regression_tests/tests/prom/dg_advection_local_rom_matrix_interp.sh
+++ regression_tests/tests/prom/dg_advection_local_rom_matrix_interp.sh
@@ -3,12 +3,14 @@ source $GITHUB_WORKSPACE/regression_tests/common.sh
 
 CMDS=( 
     "rm -rf frequencies.txt" 
-    "./dg_advection_local_rom_matrix_interp --mesh ../data/periodic-square.mesh -offline -rs 4 -ff 1.02"
-    "./dg_advection_local_rom_matrix_interp --mesh ../data/periodic-square.mesh-interp_prep -rs 4 -ff 1.02 -rdim 40"
-    "./dg_advection_local_rom_matrix_interp --mesh ../data/periodic-square.mesh -offline -rs 4 -ff 1.08"
-    "./dg_advection_local_rom_matrix_interp --mesh ../data/periodic-square.mesh -interp_prep -rs 4 -ff 1.08 -rdim 40"
-    "./dg_advection_local_rom_matrix_interp --mesh ../data/periodic-square.mesh -fom -rs 4 -ff 1.05 -visit"
-    "./dg_advection_local_rom_matrix_interp --mesh ../data/periodic-square.mesh -online_interp -rs 4 -ff 1.05 -rdim 40"
+    "./dg_advection_local_rom_matrix_interp -offline -ff 1.0"
+    "./dg_advection_local_rom_matrix_interp -interp_prep -ff 1.0 -rdim 40"
+    "./dg_advection_local_rom_matrix_interp -offline -ff 1.1"
+    "./dg_advection_local_rom_matrix_interp -interp_prep -ff 1.1 -rdim 40"
+    "./dg_advection_local_rom_matrix_interp -offline -ff 1.2"
+    "./dg_advection_local_rom_matrix_interp -interp_prep -ff 1.2 -rdim 40"
+    "./dg_advection_local_rom_matrix_interp -fom -ff 1.15"
+    "./dg_advection_local_rom_matrix_interp -online_interp -ff 1.15 -rdim 40"
 )
 TYPE="PROM"
 OFFSET=0
