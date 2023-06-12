a261c1d3cf649355f37fb82b07a4f0b2c5e6d11b
Axel Larsson <65452706+axla-io@users.noreply.github.com>
2023-06-12 23:41:20 +0200

    perhaps segfault fix

--- examples/prom/nonlinear_elasticity_global_rom_eqp.hpp
+++ examples/prom/nonlinear_elasticity_global_rom_eqp.hpp
@@ -419,7 +419,7 @@ void ComputeElementRowOfG(const IntegrationRule *ir, Array<int> const &vdofs,
     Vector el_vect(dof * dim);
 
     // Get nonlinear operator model
-    NonlinearForm *nl_H = oper.H;
+    NonlinearForm nl_H = *oper.H;
     //DomainNonlinearForm *dnf = nl_H->GetDNF();
     //NonlinearFormIntegrator* dnf = (*(nl_H->GetDNFI()))[0];
     Array<NonlinearFormIntegrator*>* dnfis = nl_H->GetDNFI();
@@ -656,11 +656,13 @@ void SetupEQP_snapshots(const IntegrationRule *ir0, const int rank,
                 // Compute the row of G corresponding to element e, store in r
                 // Create vector r_test which will be compared to the nonlinear hyperelastic operator
                 
-                ComputeElementRowOfG(ir0, vdofs, hi_gf, vj_gf, oper, model, elfun, fe, *eltrans, r);
+                ComputeElementRowOfG(ir0, vdofs, hi_gf, vj_gf, oper, model, elfun, fe, *eltrans, r, el_vect);
 
                 for (int m = 0; m < nqe; ++m)
                     Gt((e * nqe) + m, j + (i * NB)) = r[m];
             }
+
+
         }
 
         if (precondition)
