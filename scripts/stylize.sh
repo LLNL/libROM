83f6ef57b20f91a5e782ed50379dd901542468d9
Kevin Chung <chung28@llnl.gov>
2023-07-07 18:13:22 -0700

    scripts/stylize.sh: by default, checks if stylization is required. enforces the stylization by -f flag.

--- scripts/stylize.sh
+++ scripts/stylize.sh
@@ -1,16 +1,68 @@
-if [ "$1" == "" ] || [ $# -gt 1 ]; then
-    echo "Usage: ./stylize.sh /path/to/astyle/executable"
-    exit 0
+if [ "$1" == "" ] || [ $# -gt 2 ]; then
+    echo "Usage: ./stylize.sh [-f] /path/to/astyle/executable"
+    echo "Checks if stylization is required."
+    echo "  -f: enforce stylization. By default, only the check is executed."
+    exit 1
+fi
+ASTYLE_BIN=$1
+enforce=false
+
+# parse the flags.
+while getopts f: flag
+do
+    case "${flag}" in
+        f)
+            enforce=true
+            ASTYLE_BIN=$2
+            ;;
+    *)
+        echo "Unknown option."
+        exit 1
+      ;;
+    esac
+done
+
+# astyle version check
+ASTYLE_VER="Artistic Style Version 3.1"
+astyle_version="$($ASTYLE_BIN --version)"
+if [ "$astyle_version" != "$ASTYLE_VER" ]; then
+    printf "%s\n" "Invalid astyle version: '$astyle_version'"\
+            "Please use: '$ASTYLE_VER'"
+    exit 1
+fi
+
+# astyle dry-run if not enforced.
+if [ $enforce != true ]; then
+    ASTYLE_BIN="$ASTYLE_BIN --dry-run"
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DIR="$(dirname "$DIR")"
 
-$1 --recursive --max-code-length=80 "$DIR/lib/*.cpp"
-$1 --recursive --max-code-length=80 "$DIR/lib/*.h"
-$1 --recursive --max-code-length=80 "$DIR/lib/*.hpp"
-$1 --recursive --max-code-length=80 "$DIR/lib/*.c"
-$1 --recursive --max-code-length=80 "$DIR/lib/*h.in"
-$1 --recursive --max-code-length=80 "$DIR/tests/*.cpp"
-$1 --recursive --max-code-length=80 "$DIR/examples/*.cpp"
-$1 --recursive --max-code-length=80 "$DIR/examples/*.hpp" 
+FILELIST=("lib/*.cpp"
+          "lib/*.h"
+          "lib/*.hpp"
+          "lib/*.c"
+          "lib/*h.in"
+          "regression_tests/*.cpp"
+          "unit_tests/*.cpp"
+          "examples/*.cpp"
+          "examples/*.hpp")
+
+ASTYLE_COMMAND="$ASTYLE_BIN --recursive --max-code-length=80"
+
+result=false
+for files in ${FILELIST[@]}
+do
+    if $ASTYLE_COMMAND "$DIR/$files" | grep "Formatted"; then
+        result=true
+    fi
+done
+
+if [ $result = true ]; then
+    echo "Files need stylization!"
+    echo "Please run stylization before merging the pull-request."
+    echo "    1. Install $ASTYLE_VER"
+    echo "    2. cd scripts && ./stylize.sh -f /path/to/astyle"
+    exit 1
+fi
