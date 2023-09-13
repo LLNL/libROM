e6cfaa588c9fc971fd9875a56ea2f8afd8f91cbb
Dylan Copeland <copeland11@llnl.gov>
2023-09-13 16:06:28 -0700

    Update regression tests.

--- regression_tests/tests/prom/mixed_nonlinear_diffusion.sh
+++ regression_tests/tests/prom/mixed_nonlinear_diffusion.sh
@@ -3,8 +3,8 @@ source $GITHUB_WORKSPACE/regression_tests/common.sh
 CMDS=( 
     "./mixed_nonlinear_diffusion -offline -tf 0.01"
     "./mixed_nonlinear_diffusion -merge -ns 1 -tf 0.01"
-    "./mixed_nonlinear_diffusion -online -tf 0.01 -rrdim 2 -rwdim 2 -nldim 3"
-    "./mixed_nonlinear_diffusion -online -tf 0.01 -rrdim 2 -rwdim 2 -nldim 3 -sopt"
+    "./mixed_nonlinear_diffusion -online -tf 0.01 -rrdim 2 -rwdim 2 -nldim 3 -hrtype deim"
+    "./mixed_nonlinear_diffusion -online -tf 0.01 -rrdim 2 -rwdim 2 -nldim 3 -hrtype sopt"
     "./mixed_nonlinear_diffusion -online -tf 0.01 -rrdim 2 -rwdim 2 -nldim 3 -ns 1 -eqp -maxnnls 4"
 )
 TYPE="PROM"
