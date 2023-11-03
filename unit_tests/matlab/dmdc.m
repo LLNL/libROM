f88308e3a88a0c971726362d5b92bae3a8abc4fd
Siu Wun Cheung <cheung26@llnl.gov>
2023-11-02 19:51:13 -0700

    Replace reduced dimension by energy fraction in MATLAB

--- unit_tests/matlab/dmdc.m
+++ unit_tests/matlab/dmdc.m
@@ -1,7 +1,7 @@
-function dmdc(X, Y, r, t, dt, varargin)
+function dmdc(X, Y, ef, t, dt, varargin)
 % Input:      X: n by m snapshot matrix
 %             Y: n by (m-1) control snapshot matrix
-%             r: number of basis_vectors to use
+%            ef: energy fraction
 %             t: The time to predict
 %            dt: delta time
 %             B: (optional) known control matrix
@@ -12,12 +12,16 @@ function dmdc(X, Y, r, t, dt, varargin)
 
     if nargin == 5
         X_in = [X_in; Y];
-    else
+    elseif nargin == 6
         B = varargin{1};
         X_out = X_out - B*Y;
+    else
+        error('Invalid input');
     end
 
     [U, S, V] = svd(X_in, 'econ');
+    r = sum(cumsum(diag(S))/sum(diag(S)) < ef)
+    
     U = U(:,1:r);
     S = S(1:r,1:r);
     V = V(:,1:r);
@@ -26,15 +30,14 @@ function dmdc(X, Y, r, t, dt, varargin)
     U1 = U(1:m,:);
     if nargin == 5
         U2 = U(m+1:end,:);
-        r_out = r-1;
-        [U_out, ~, ~] = svd(X_out, 'econ');
+        [U_out, S_out, ~] = svd(X_out, 'econ');
+        r_out = sum(cumsum(diag(S_out))/sum(diag(S_out)) < ef)
         U_out = U_out(:,1:r_out);
         Atilde = U_out'*X_out*V*inv(S)*U1'*U_out;
         Btilde = U_out'*X_out*V*inv(S)*U2';
         [W,ev] = eig(Atilde);
         %Phi = X_out*V*inv(S)*U1'*U_out*W;
     else
-        r_out = r;
         U_out = U1;
         Atilde = U_out'*X_out*V*inv(S);
         Btilde = U_out'*B;
