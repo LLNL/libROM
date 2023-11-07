16ab29f9286e41c3127a53a366ed8bb2e9f9c3e7
William Anderson <53445030+andersonw1@users.noreply.github.com>
2023-11-07 08:39:42 -0800

    Update maxwell_global_rom.cpp

--- examples/prom/maxwell_global_rom.cpp
+++ examples/prom/maxwell_global_rom.cpp
@@ -68,9 +68,9 @@ int main(int argc, char *argv[])
     Mpi::Init(argc, argv);
     int num_procs = Mpi::WorldSize();
     int myid = Mpi::WorldRank();
+    Hypre::Init();
 
     // 2. Parse command-line options.
-//   const char *mesh_file = "../data/beam-tet.mesh";
     const char *mesh_file = "../data/star.mesh";
     int order = 1;
     bool static_cond = false;
