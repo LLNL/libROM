fc51f9e493843e3bd967fe7f80d176cd2898a3fb
Kevin Chung <dreamer2368@gmail.com>
2024-02-16 14:58:18 -0800

    hdf5 parallel integer array reading example.

--- unit_tests/test_HDFDatabase.cpp
+++ unit_tests/test_HDFDatabase.cpp
@@ -38,7 +38,7 @@ TEST(HDF5, Test_parallel_writing)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     const int dim_rank = 2;
-    const int nrow = 10000, ncol = 400;
+    const int nrow = 1000, ncol = 400;
     const int nrow_local = CAROM::split_dimension(nrow, MPI_COMM_WORLD);
     std::vector<int> offsets;
     int dummy = CAROM::get_global_offsets(nrow_local, offsets, MPI_COMM_WORLD);
@@ -56,7 +56,7 @@ TEST(HDF5, Test_parallel_writing)
     hid_t plist_id;
     hid_t file_id;
     herr_t errf = 0;
-    
+ 
     /*
      * Set up file access property list with parallel I/O access
      */
@@ -136,6 +136,121 @@ TEST(HDF5, Test_parallel_writing)
     const auto stop = steady_clock::now();
     const auto duration = duration_cast<milliseconds>(stop-start);
     printf("rank: %d, duration: %dms\n", rank, duration);
+}
+
+TEST(HDF5, Test_parallel_reading)
+{
+    int nproc, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    const int dim_rank = 2;
+    const int nrow = 1000, ncol = 400;
+    const int nrow_local = CAROM::split_dimension(nrow, MPI_COMM_WORLD);
+    std::vector<int> offsets;
+    int dummy = CAROM::get_global_offsets(nrow_local, offsets, MPI_COMM_WORLD);
+
+    /*
+     * Initialize data buffer
+     */
+    int data[nrow_local * ncol];
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    const auto start = steady_clock::now();
+
+    hid_t plist_id;
+    hid_t file_id;
+    herr_t errf = 0;
+    
+    /*
+     * Set up file access property list with parallel I/O access
+     */
+    plist_id = H5Pcreate(H5P_FILE_ACCESS);
+    H5Pset_fapl_mpio(plist_id, MPI_COMM_WORLD, MPI_INFO_NULL);
+
+    /*
+     * OPTIONAL: It is generally recommended to set collective
+     *           metadata reads/writes on FAPL to perform metadata reads
+     *           collectively, which usually allows datasets
+     *           to perform better at scale, although it is not
+     *           strictly necessary.
+     */
+    H5Pset_all_coll_metadata_ops(plist_id, true);
+    H5Pset_coll_metadata_write(plist_id, true);
+    
+    /*
+     * Open a new file collectively and release property list identifier.
+     */
+    file_id = H5Fopen("test.h5", H5F_ACC_RDONLY, plist_id);
+    H5Pclose(plist_id);
+
+    /*
+     * Open the dataset with default properties.
+     */
+    hid_t dset_id = H5Dopen(file_id, "test-int", H5P_DEFAULT);
+
+    /*
+     * Get filespace and read the global size.
+     */
+    hid_t filespace = H5Dget_space(dset_id);
+    CAROM_VERIFY(filespace >= 0);
+    int ndims = H5Sget_simple_extent_ndims(filespace);
+    CAROM_VERIFY(ndims == dim_rank);
+    hsize_t dim_global[dim_rank];
+    errf = H5Sget_simple_extent_dims(filespace, dim_global, NULL);
+    CAROM_VERIFY(errf >= 0);
+    CAROM_VERIFY(dim_global[0] == nrow);
+    CAROM_VERIFY(dim_global[1] == ncol);
+
+    H5Sclose(filespace);
+
+    /*
+     * Each process defines dataset in memory and writes it to the hyperslab
+     * in the file.
+     */
+    /* hyperslab selection parameters */
+    hsize_t count[dim_rank];            
+    hsize_t offset[dim_rank];
+    count[0]  = nrow_local;
+    count[1]  = ncol;
+    offset[0] = offsets[rank];
+    offset[1] = 0;
+    hid_t memspace  = H5Screate_simple(dim_rank, count, NULL);
+
+    /*
+     * Select hyperslab in the file.
+     */
+    filespace = H5Dget_space(dset_id);
+    H5Sselect_hyperslab(filespace, H5S_SELECT_SET, offset, NULL, count, NULL);
+
+    /*
+     * Create property list for collective dataset write.
+     */
+    plist_id = H5Pcreate(H5P_DATASET_XFER);
+    H5Pset_dxpl_mpio(plist_id, H5FD_MPIO_COLLECTIVE);
+
+    /*
+     * Read the data collectively.
+     */
+    errf = H5Dread(dset_id, H5T_NATIVE_INT, memspace, filespace, plist_id, data);
+    CAROM_VERIFY(errf >= 0);
+
+    /*
+     * Close/release resources.
+     */
+    H5Dclose(dset_id);
+    H5Sclose(filespace);
+    H5Sclose(memspace);
+    H5Pclose(plist_id);
+    H5Fclose(file_id);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    const auto stop = steady_clock::now();
+    const auto duration = duration_cast<milliseconds>(stop-start);
+    printf("rank: %d, duration: %dms\n", rank, duration);
+
+    for (int d = 0; d < nrow_local * ncol; d++)
+        EXPECT_TRUE(data[d] == d + offsets[rank] * ncol);
 }
 
 int main(int argc, char* argv[])
