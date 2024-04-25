a12a6456ebab5a1424b3726c0def24d8db7d3004
ptranq <tranquilli1@llnl.gov>
2024-04-25 14:06:26 -0700

    Stylize

--- lib/algo/manifold_interp/SnapshotInterpolator.cpp
+++ lib/algo/manifold_interp/SnapshotInterpolator.cpp
@@ -10,7 +10,7 @@
 
 /**
  * Implements PCHIP algorithm.  Based on "A METHOD FOR CONSTRUCTING LOCAL MONOTONE
- * PIECEWISE CUBIC INTERPOLANTS", Fritchs and Butland (1984).  as well as "MONOTONE 
+ * PIECEWISE CUBIC INTERPOLANTS", Fritchs and Butland (1984).  as well as "MONOTONE
  * PIECEWISE CUBIC INTERPOLATION," Fritsch and Carlson (1980)
  *
  */
