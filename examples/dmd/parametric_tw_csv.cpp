e4646fa2471250a4cd3264afcdf4f45906840e25
Siu Wun Cheung <cheung26@llnl.gov>
2024-06-27 07:02:08 -0700

    Set default options of DMD

--- examples/dmd/parametric_tw_csv.cpp
+++ examples/dmd/parametric_tw_csv.cpp
@@ -598,7 +598,7 @@ int main(int argc, char *argv[])
                 }
                 else if (dtc > 0.0)
                 {
-                    dmd_curr_par[window] = new CAROM::DMD(dim, dtc, true);
+                    dmd_curr_par[window] = new CAROM::DMD(dim, dtc);
                 }
                 else
                 {
